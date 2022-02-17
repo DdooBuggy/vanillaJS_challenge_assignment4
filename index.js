219d3b80aa91de6309fbbd931abda7482a9d8ea6
DdooBuggy <yhiyo03@gmail.com>
2022-02-17 18:56:19 +0900

    recreate calc

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ function getDate() {
     const date = new Date();
     const curYear = date.getFullYear();
     const Dday = new Date(`${curYear}-12-25T00:00:00`);
-    const distance = Dday - date;
-    const distanceSecond = String(Math.floor((distance % (1000*60))/1000)).padStart(2, "0");
-    const distanceMinute = String(Math.floor((distance % (1000*60*60))/(1000*60))).padStart(2, "0");
-    const distanceHour = String(Math.floor((distance % (1000*60*60*24))/(1000*60*60))).padStart(2, "0");
-    const distanceDate = Math.floor(distance/(1000*60*60*24));
+    const distance = Math.floor((Dday - date)/1000);
+    const distanceSecond = String(Math.floor(distance % 60)).padStart(2, "0");
+    const distanceMinute = String(Math.floor((distance/60) % 60)).padStart(2, "0");
+    const distanceHour = String(Math.floor((distance/60/60)%24)).padStart(2, "0");
+    const distanceDate = Math.floor(distance/60/60/24);
     timer.innerHTML = `${distanceDate}d ${distanceHour}h ${distanceMinute}m ${distanceSecond}s`;
 }
 getDate();
